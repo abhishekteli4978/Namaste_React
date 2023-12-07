6130c3e90f387e375c3e43753f5d81ea8bdd65c8
Abhishek <abhishek4978@gmail.com>
2023-12-07 07:18:48 +0530

    Second update

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // const heading=React.createElement('h1',{},'NAsmaste Duniya from react!!');
 
+import React from "react";
+import ReactDOM from "react-dom/client"
 
 const parent = React.createElement("div",{id :"parent"},
 [React.createElement("div",{id : "child1"},[
-    React.createElement("h1",{},"Iam h1 tag"),
+    React.createElement("h1",{},"Its Abhi learning NAmaste React"),
     React.createElement("h2",{},"Iam h2 tag!")
 ]),
 React.createElement("div",{id : "child1"},[
